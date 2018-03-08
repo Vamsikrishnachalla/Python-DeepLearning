5a858387a8b4419b27612068fb1c462d4fc2c807
Vamsi Krishna Challa <vamsikrishna.challa544@gmail.com>
2018-03-08 00:51:28 -0600

    Assignment 3

--- lab3/Source/assignment3/lda.py
+++ lab3/Source/assignment3/lda.py
@@ -1,36 +1,32 @@
-import matplotlib.pyplot as plt
 
-from sklearn import datasets
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-
-iris = datasets.load_iris()
-
-X = iris.data
-y = iris.target
-target_names = iris.target_names
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+from sklearn.datasets import load_digits
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+import matplotlib.pyplot as plot
 
-from sklearn.neighbors import KNeighborsClassifier
-classifier = KNeighborsClassifier(n_neighbors=5)
-classifier.fit(X_train, y_train)
+# Loading the digits data into variable
+db=load_digits()
+ctn=db.data
+ar1=db.target
+sl, sw, pl, pw = train_test_split(ctn, ar1, test_size=0.2, random_state=30)
+# Now let us perform LDA
+ldperform = LinearDiscriminantAnalysis()
+# Here we are performing regression with logistic orientation #logistic=LogisticRegression()
+a = ldperform.fit(sl,pl)
+yprediction = ldperform.predict(sw) #Predicting the outcomes
+#logistic.fit(sl, pl)
+# Now here we perform outcome predicting process.
+yprediction=ldperform.predict(sw)
 
-y_pred = classifier.predict(X_test)
+# let us Calculate accuracy score with applyication of LinearDiscriminantAnalysis model
+print("Accuracy of Linear discriminant analysis:", accuracy_score(yprediction, pw))
 
-
-lda = LinearDiscriminantAnalysis(n_components=2)
-X_r2 = lda.fit(X_test, y_pred).transform(X)
-
-plt.figure()
-colors = ['navy', 'turquoise', 'darkorange']
-lw = 2
-
-
-for color, i, target_name in zip(colors, [0, 1, 2], target_names):
-    plt.scatter(X_r2[y == i, 0], X_r2[y == i, 1], alpha=.8, color=color,
-                label=target_name)
-plt.legend(loc='best', shadow=False, scatterpoints=1)
-plt.title('LDA of IRIS dataset')
-
-plt.show()
+plot.figure()
+col = ['green', 'red', 'blue']
+for x, y, z in zip(col, [0, 1, 2], db):
+    plot.scatter(a[ar1 == y, 0], a[ar1 == y, 1], alpha=.8, color=x,label=z)
+plot.legend(loc='best', shadow=False, scatterpoints=1)
+plot.title('Linear Discriminant Analysis results performed on Iris data set:')
+plot.show()
